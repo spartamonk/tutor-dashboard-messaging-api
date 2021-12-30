c08d261c8b9882458025c85a4b0ec4e3eba18bae
Christopher Akoto <71773056+spartamonk@users.noreply.github.com>
2021-12-29 21:36:08 -0600

    updated put request

--- functions/1-students.js
+++ functions/1-students.js
@@ -7,48 +7,98 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
 
 exports.handler = async (event, context, cb) => {
   const { id } = event.multiValueQueryStringParameters
-  if (id) {
-    try {
-      const student = await airtable.retrieve(id)
-     
+  const method = event.httpMethod;
+ if(method === 'GET') {
+if (id) {
+  try {
+    const student = await airtable.retrieve(id)
 
-      if (student.error) {
-        return {
-          headers: { 'Access-Control-Allow-Origin': '*' },
-          statusCode: 404,
-          body: `No student with id: "${id}"`,
-        }
-      }
+    if (student.error) {
       return {
         headers: { 'Access-Control-Allow-Origin': '*' },
-        statusCode: 200,
-        body: JSON.stringify(student),
-      }
-    } catch (error) {
-      return {
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        statusCode: 500,
-        body: 'Server error',
+        statusCode: 404,
+        body: `No student with id: "${id}"`,
       }
     }
+    return {
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      statusCode: 200,
+      body: JSON.stringify(student),
+    }
+  } catch (error) {
+    return {
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      statusCode: 500,
+      body: 'Server error',
+    }
   }
-  try {
-    const { records } = await airtable.list({ maxRecords: 400 })
-    const students = records.map((i) => {
-      const { id } = i
-      const {
-        firstName,
-        lastName,
-        image,
-        date,
-        lessonRequest,
-        adminMessageSummary,
-        message,
-        adminMessage,
-        adminMessageResolved,
+}
+try {
+  const { records } = await airtable.list({ maxRecords: 400 })
+  const students = records.map((i) => {
+    const { id } = i
+    const {
+      firstName,
+      lastName,
+      image,
+      date,
+      lessonRequest,
+      adminMessageSummary,
+      message,
+      adminMessage,
+      adminMessageResolved,
+      subject,
+      level,
+      contact,
+      online,
+      lessonRequirements,
+      requestStatus,
+      startTime,
+      endTime,
+      rate,
+      tutorAddress,
+      address,
+      postcode,
+      statusColor,
+      notification1,
+      notification2,
+      weekly,
+      requestMessage,
+      starred,
+      unread,
+      archived,
+      requestAction1,
+      requestAction1Color,
+      requestAction1BgColor,
+      requestAction1BorderColor,
+      requestAction1ClassName,
+      requestAction2,
+      requestAction2Color,
+      requestAction2BgColor,
+      requestAction2BorderColor,
+      requestAction2ClassName,
+      adminCaseResolveMessage,
+    } = i.fields
+    const imageUrl = image[0].url
+    return {
+      id,
+      firstName,
+      lastName,
+      imageUrl,
+      date,
+      contact,
+      adminMessage,
+      adminMessageResolved,
+      adminCaseResolveMessage,
+      lessonRequest,
+      message,
+      starred,
+      unread,
+      archived,
+      adminMessageSummary,
+      lessonDetails: {
         subject,
         level,
-        contact,
         online,
         lessonRequirements,
         requestStatus,
@@ -59,87 +109,69 @@ exports.handler = async (event, context, cb) => {
         address,
         postcode,
         statusColor,
-        notification1,
-        notification2,
         weekly,
         requestMessage,
-        starred,
-        unread,
-        archived,
-        requestAction1,
-        requestAction1Color,
-        requestAction1BgColor,
-        requestAction1BorderColor,
-        requestAction1ClassName,
-        requestAction2,
-        requestAction2Color,
-        requestAction2BgColor,
-        requestAction2BorderColor,
-        requestAction2ClassName,
-        adminCaseResolveMessage,
-      } = i.fields
-      const imageUrl = image[0].url
-      return {
-        id,
-        firstName,
-        lastName,
-        imageUrl,
-        date,
-        contact,
-        adminMessage,
-        adminMessageResolved,
-        adminCaseResolveMessage,
-        lessonRequest,
-        message,
-        starred,
-        unread,
-        archived,
-        adminMessageSummary,
-        lessonDetails: {
-          subject,
-          level,
-          online,
-          lessonRequirements,
-          requestStatus,
-          startTime,
-          endTime,
-          rate,
-          tutorAddress,
-          address,
-          postcode,
-          statusColor,
-          weekly,
-          requestMessage,
-          requestActions: [
-            {
-              requestAction1,
-              requestAction1Color,
-              requestAction1BgColor,
-              requestAction1BorderColor,
-              requestAction1ClassName,
-            },
-            {
-              requestAction2,
-              requestAction2Color,
-              requestAction2BgColor,
-              requestAction2BorderColor,
-              requestAction2ClassName,
-            },
-          ],
-        },
-        initialNotifications: [notification1, notification2],
-      }
-    })
-    return {
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      statusCode: 200,
-      body: JSON.stringify(students),
+        requestActions: [
+          {
+            requestAction1,
+            requestAction1Color,
+            requestAction1BgColor,
+            requestAction1BorderColor,
+            requestAction1ClassName,
+          },
+          {
+            requestAction2,
+            requestAction2Color,
+            requestAction2BgColor,
+            requestAction2BorderColor,
+            requestAction2ClassName,
+          },
+        ],
+      },
+      initialNotifications: [notification1, notification2],
     }
-  } catch (error) {
-    return {
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      statusCode: 500,
-      body: 'Server error',
+  })
+  return {
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    statusCode: 200,
+    body: JSON.stringify(students),
+  }
+} catch (error) {
+  return {
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    statusCode: 500,
+    body: 'Server error',
+  }
+}
+ }
+  if(method === 'PUT') {
+    
+    try {
+      const { id, unread } = JSON.parse(event.body);
+      console.log(id, unread);
+      if(!id || !unread) {
+        return {
+          statusCode: 400,
+          body: 'Please pass id and unread values'
+        }
+      }
+      const fields ={unread: false}
+      const item = await airtable.update(id, {fields});
+      if(item.error) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify(item)
+        }
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(item),
+      }
+    } catch (error) {
+      return {
+        statusCode: 400,
+        body: "Please pass id and unread values"
+      }
     }
   }
 }
